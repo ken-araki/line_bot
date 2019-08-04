42db6c8c46b0edf25283055d725c739a6f0cb4be
k_araki <ken.araki.61@gmail.com>
2019-08-04 21:33:36 +0900

    bug fixed

--- src/main/java/com/line/LineApiApplication.java
+++ src/main/java/com/line/LineApiApplication.java
@@ -6,11 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
 @SpringBootApplication
+@LineMessageHandler
 public class LineApiApplication {
 
 	public static void main(String[] args) {
@@ -18,14 +19,13 @@ public class LineApiApplication {
 	}
 	
 	@EventMapping
-	public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-		// System.out.println("event: " + event);
-		final String originalMessageText = event.getMessage().getText();
-		return new TextMessage(originalMessageText);
+	public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+		System.out.println("event: " + event);
+		return new TextMessage(event.getMessage().getText());
 	}
 
 	@EventMapping
 	public void handleDefaultMessageEvent(Event event) {
-		// System.out.println("event: " + event);
+		System.out.println("event: " + event);
 	}
 }
