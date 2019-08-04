1132552737ec0508ad46a344244fd7fc936cc357
k_araki <ken.araki.61@gmail.com>
2019-08-04 20:54:56 +0900

    first commit

---
+++ src/main/java/com/line/LineApiApplication.java
@@ -0,0 +1,31 @@
+package com.line;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+
+@SpringBootApplication
+public class LineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineApiApplication.class, args);
+	}
+	
+	@EventMapping
+	public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+		System.out.println("event: " + event);
+		final String originalMessageText = event.getMessage().getText();
+		return new TextMessage(originalMessageText);
+	}
+
+	@EventMapping
+	public void handleDefaultMessageEvent(Event event) {
+		System.out.println("event: " + event);
+	}
+}
