7fc92917b7a23bc84b33838cccd4523a7db2415a
ken-araki <k.takagaki.614@gmail.com>
2019-09-09 16:28:35 +0900

    [fix][remove][-]herokuのフル稼働はHeroku Schedulerを利用することにしたので削除しました

--- src/main/java/com/linebot/config/LineProperties.java
+++ src/main/java/com/linebot/config/LineProperties.java
@@ -12,24 +12,10 @@ import lombok.Setter;
 @ConfigurationProperties(prefix = "ark.line")
 public class LineProperties {
 	private String id;
-	private String host;
-	private String port;
 	public String getId() {
 		return this.id;
 	}
 	public void setId(String id) {
 		this.id = id;
-	}
-	public String getHost() {
-		return this.host;
-	}
-	public void setHost(String host) {
-		this.host = host;
-	}
-	public String getPort() {
-		return this.port;
-	}
-	public void setPort(String port) {
-		this.port = port;
 	}
 }
