5e006fcd11baa9f79cd90e9b999cb3836a90225e
ken-araki <k.takagaki.614@gmail.com>
2019-10-23 00:38:58 +0900

    [fix][clean][-]TestControllerをlocal(デバッグ)用にしました

--- src/main/java/com/linebot/controller/test/TobuyController.java
+++ src/main/java/com/linebot/controller/test/TobuyController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping(name = "/test/tobuy")
+@Profile("local")
 public class TobuyController {
     private ManageTobuyAction manageTobuyAction;
 
