180298f962fba475aa769ef618b044af93999793
ken-araki <k.takagaki.614@gmail.com>
2019-11-01 00:41:19 +0900

    月末の場合通知されないバグを修正しました。(合わせてロジックをサービスへ切り出し)

--- src/main/java/com/linebot/batch/NoticeGarbageOutBatch.java
+++ src/main/java/com/linebot/batch/NoticeGarbageOutBatch.java
@@ -1,34 +1,19 @@
 package com.linebot.batch;
 
-import java.util.Collections;
-import java.util.concurrent.ExecutionException;
-
-import com.linebot.util.Utils;
+import com.linebot.service.NoticeGarbageOutService;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
-
-import com.linecorp.bot.client.LineMessagingClient;
-import com.linecorp.bot.model.Broadcast;
-import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.model.response.BotApiResponse;
-
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @AllArgsConstructor
 @Controller
 public class NoticeGarbageOutBatch {
-	private LineMessagingClient lineMessagingClient;
+	private NoticeGarbageOutService noticeGarbageOutService;
 
-	@Scheduled(cron = "0 20 21 1-7,15-21 * 4", zone = "Asia/Tokyo")
-	public void executeResourcesPrevious() {
-		log.info("exec executeResourcesPrevious()");
-		final BotApiResponse response = Utils.uncheck(() -> {
-			return lineMessagingClient.broadcast(new Broadcast(Collections.singletonList(
-					new TextMessage("明日は資源ごみの日です。")), false)).get();
-		});
-		log.info("Sent messages: {}", response);
+	@Scheduled(cron = "0 20 21 * * 4", zone = "Asia/Tokyo")
+	public void execute() {
+		noticeGarbageOutService.executeDayBefore();
 	}
 }
---
+++ src/main/java/com/linebot/service/NoticeGarbageOutService.java
@@ -0,0 +1,51 @@
+package com.linebot.service;
+
+import com.linebot.util.Utils;
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.Broadcast;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.function.BiFunction;
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class NoticeGarbageOutService {
+    private LineMessagingClient lineMessagingClient;
+
+    public void executeDayBefore() {
+        if (this.isTommorowFirstOrThirdFriday()) {
+            return;
+        }
+        final BotApiResponse response = Utils.uncheck(() -> {
+            return lineMessagingClient.broadcast(new Broadcast(Collections.singletonList(
+                    new TextMessage("明日は資源ごみの日です。")), false)).get();
+        });
+    }
+
+    public boolean checkDay(int add, BiFunction<Integer, DayOfWeek, Boolean> fn) {
+        LocalDateTime target = LocalDateTime.now().plusDays(add);
+        DayOfWeek dayOfWeek = target.getDayOfWeek();
+        int day = target.getDayOfMonth();
+        return (Boolean) fn.apply(day, dayOfWeek);
+    }
+
+    /**
+     * ゴミ出し前日であるかチェックする
+     * Spring-Scheduleで木曜日チェック済みだが、別ルートで叩くことも考慮しここでもチェックする
+     */
+    public boolean isTommorowFirstOrThirdFriday() {
+        return checkDay(1, (day, dayOfWeek) -> {
+                    return DayOfWeek.THURSDAY == dayOfWeek && ((1 <= day && day <= 7) || (15 <= day && day <= 21));
+                }
+        );
+    }
+
+}
